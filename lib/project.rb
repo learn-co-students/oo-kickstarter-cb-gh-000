7ebaaa4560942c3a5e9ee6ca9bd35f89584d0021
Dídac González <didac.gonza@gmail.com>
2017-02-08 19:04:33 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(person)
+    @backers << person
+    person.backed_projects << self
+  end
+
+end
