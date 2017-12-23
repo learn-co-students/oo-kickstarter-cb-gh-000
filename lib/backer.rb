5c8aa4dd4a41cd83bb290f91cae8f1cb67926e6d
Gene Gi <junzheji@gmail.com>
2017-12-23 06:13:08 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,17 @@
 class Backer
-  attr_accessor :backed_projects
+  attr_accessor :name, :backed_projects
 
-  def initialize(backed_projects=[])
-    @backed_projects = backed_projects
+
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
   end
+
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self)
+  end
+
+
 end
