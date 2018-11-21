69b23308bf9f3111ca9dfc418b9202dfc369ddc3
Joseph Corona <josed.hc96@gmail.com>
2018-11-21 04:56:49 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+
+  end
+
+  attr_accessor :name, :backed_projects
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+
+  def initialize(title)
+    @title = title
+    @backers = []
+
+  end
+
+  attr_accessor :title, :backers
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+
+  end
+end
