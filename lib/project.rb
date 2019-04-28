0c412f3febb8bfbf6b14578189c505e3264db26f
Bryan Jimenez <bryan@roguedevs.io>
2019-04-28 08:08:22 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+  attr_reader :title
+  attr_accessor :backers, :backer
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def backers
+    @backers
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+  
+end
